9b74f5fde2eaafb2e00d6d0b112cdc48761c5d3a
Tez Nebraska <teznebraska@gmail.com>
2025-07-04 21:30:31 -0500

    commiting changes to prepare for upload to github

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,5 @@
 const RPC_URL = "https://ghostnet.ecadinfra.com";
 
-const CONTRACT_ADDRESS = "KT1APJfwx4fzzDRPr6W37BWZJ25cwad2piTn";
+const CONTRACT_ADDRESS = "KT1Dtac3Hook36mQFS7cH1nSDgGbn7iVFwug";
 
 export { RPC_URL, CONTRACT_ADDRESS };
